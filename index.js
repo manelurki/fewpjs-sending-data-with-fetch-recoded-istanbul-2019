6c88a1208974591eb97b434710aa026e212a84cd
manel turki <menelturki@gmail.com>
2020-02-19 19:08:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(){
+  let user={};
+  user["name"]='  Steve';
+  user["email"]='steve@gmail.com';
+  let configObj={
+    method:"POST",
+    headers: {
+      "Content-Type":"application/json","Accept":"application/json"},
+     body: JSON.stringify(user),
+};
+    
+  
+   return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let h=document.createElement('h2');
+    h.innerHTML=object.id;
+    document.body.appendChild(h);
+    console.log(object);
+  })
+  .catch(function(error) {
+   let h=document.createElement('h2');
+    h.innerHTML=error.message;
+    document.body.appendChild(h);
+  });
+}
+submitData();
